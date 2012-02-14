2e28164c1cc3e1153711955a48f54f4aa45c798c
Robert David Grant <robert.david.grant@gmail.com>
2012-02-14 01:40:48 -0600

    Clean up for push to github.

--- hackdiet.R
+++ hackdiet.R
@@ -1,9 +1,29 @@
+#  hackdiet.R:  Weight, Body-fat, and Wakeup-time plotting inspired by
+#  the of the Hacker's Diet online by John Walker.
+#
+#  :author: Robert David Grant <robert.david.grant@gmail.com>
+#
+#  :copyright:
+#    Copyright 2012 Robert David Grant
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#    implied.  See the License for the specific language governing
+#    permissions and limitations under the License.
+
+
 require(ggplot2)
 setwd("~/documents/life/data/hackdiet")
 
 DATAFILE <- "data.tsv"
 
-######### data conversion functions ##########
 
 convert.datetime <- function(date.lst, time.lst) {
     date.with.time <- sprintf("%s %s", date.lst, time.lst)
@@ -18,56 +38,48 @@ convert.time <- function(time.lst) {
     sapply(time.lst, string.to.hour)
 }
 
-
-######### interactive functions ##########
-
+# Read and parse the data file
 read.data <- function(file=DATAFILE) {
-    wt <- read.delim(file, header=TRUE, skip=1,
+    data <- read.delim(file, header=TRUE, skip=1,
                      colClasses=c('Date', 'numeric', 'numeric', 'character'))
-    wt$Day.Length <- c(NA, diff(convert.datetime(wt$Date, wt$Wake)))
-    wt$Wake <- convert.time(wt$Wake)
-    return(wt)
+    data$Day.Length <- c(NA, diff(convert.datetime(data$Date, data$Wake)))
+    data$Wake <- convert.time(data$Wake)
+    return(data)
 }
 
-get.mydata <- function(since="1900-01-01", data=wt) {
-    sub.wt <- subset(data, Date > as.Date(since))
-    melt(sub.wt, id.vars='Date', measure.vars=c('Weight', 'BF', 'Wake'))
-    #melt(sub.wt, id.vars='Date', measure.vars=c('Weight', 'BF'))
+# Get the data since a certain date
+get.datarange <- function(data, since="1900-01-01") {
+    sub.data <- subset(data, Date > as.Date(since))
+    melt(sub.data, id.vars='Date', measure.vars=c('Weight', 'BF', 'Wake'))
 }
 
-plot.mydata <- function(since="1900-01-01", data=wt, span=1) {
-    melt.wt <- get.mydata(since=since, data)
-    p1 <- ggplot(data=melt.wt, 
+# Make a three-paneled plot showing weight, % body fat, and wakeup time
+# vs. date.
+make.plot <- function(data, since="1900-01-01", span=1) {
+    melt.data <- get.datarange(data, since=since)
+    p1 <- ggplot(data=melt.data,
             mapping=aes(Date, value))
-    p1 + geom_point(na.rm=T) + 
+    p1 + geom_point(na.rm=T) +
          geom_smooth(na.rm=T, span=span) +
          facet_grid(variable ~ ., scales="free_y") +
          opts(title="Life Data")
 }
 
 
-# other possible plots... probably not functional right now
-plot.mywake <- function(since="1900-01-01", data=wt) {
-    wt <- read.data(file="weight.tsv")
-    sub.wt <- subset(data, Date > as.Date(since))
-#    wake.time.plot <- qplot(Date, Wake, data=wt, geom="point")
-    qplot(Date, Wake, data=sub.wt, geom="point")
-#    day.length.dplot <- qplot(Day.Length[1:length(Day.Length)-1], data=wt,
-#                             geom=c("density"), xlab="Day Length (h)")
-#    day.length.boxplot <- 
-#        qplot(factor(1),Day.Length[1:length(Day.Length)-1], 
-#            data=wt, geom="boxplot", scale_x_discrete("")) + coord_flip()
-}
+######### Top-level Functions ##########
 
-plot.daylength <- function(since="1900-01-01", data=wt) {
-    wt <- read.data(file=DATAFILE)
-    sub.wt <- subset(data, Date > as.Date(since))
-    qplot(Day.Length[1:length(Day.Length)-1], data=sub.wt,
+# Read data and plot the distribution of differences between subsequent
+# wakeup-times
+plot.daylength <- function(since="1900-01-01") {
+    data <- read.data(file=DATAFILE)
+    sub.data <- subset(data, Date > as.Date(since))
+    qplot(Day.Length[1:length(Day.Length)-1], data=sub.data,
                              geom=c("density"), xlab="Day Length (h)", na.rm=T)
 }
 
-# one function to rule them all
-show.data <- function(since="1900-01-01", span=0.3) {
-    wt <- read.data(file=DATAFILE)
-    plot.mydata(since, wt, span)
+# Read data and make a three-paneled plot showing weight, % body fat,
+# and wakeup time vs. date
+plot.data <- function(since="1900-01-01", span=0.3) {
+    data <- read.data(file=DATAFILE)
+    make.plot(data, since, span)
 }
